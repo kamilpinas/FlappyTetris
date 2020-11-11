70d4e1546956130d8baa60a5ca5229665caa1047
Kamil Budzik <kamil.budzik98@gmail.com>
2020-11-11 21:52:08 +0100

    init

---
+++ app/src/main/java/com/example/ppsm2_flappy_tetris/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ppsm2_flappy_tetris;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
